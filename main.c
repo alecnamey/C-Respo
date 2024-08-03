c257dbcb0511305083886a884e958a41e24c2328
Alec Namey <144381448+alecnamey@users.noreply.github.com>
2024-08-02 20:32:01 -0700

    Testing A C project

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main(void)
 {
     printf("Hello, World!\n");
     printf("Testing Git Commands\n");
+    printf("Add a new line");
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Alec Namey on 8/2/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
